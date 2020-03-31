ffb1b622c82d8730bd1bb3badadad20dd60b5c3c
Elias Norrby <elias.norrby@gmail.com>
2020-03-31 10:00:05 +0200

    style: format with prettier

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 module.exports = {
-  dependabot: "🤖",
-};
+  dependabot: '🤖',
+}
--- setup.js
+++ setup.js
@@ -1,58 +1,58 @@
 #!/usr/bin/env node
-const path = require("path");
-const fs = require("fs");
-const yargs = require("yargs");
+const path = require('path')
+const fs = require('fs')
+const yargs = require('yargs')
 
-const { log } = require("@eliasnorrby/log-util");
+const { log } = require('@eliasnorrby/log-util')
 
 yargs
-  .alias("v", "version")
-  .usage("Usage: $0 [options]")
-  .option("f", {
-    describe: "Overwrite existing files",
-    type: "boolean",
-    alias: "force",
+  .alias('v', 'version')
+  .usage('Usage: $0 [options]')
+  .option('f', {
+    describe: 'Overwrite existing files',
+    type: 'boolean',
+    alias: 'force',
     default: false,
   })
-  .help("h")
-  .alias("h", "help")
-  .strict(true);
-const argv = yargs.argv;
+  .help('h')
+  .alias('h', 'help')
+  .strict(true)
+const argv = yargs.argv
 
 // const packageName = "@eliasnorrby/git-config";
 
-if (!fs.existsSync("package.json")) {
+if (!fs.existsSync('package.json')) {
   log.fail(
-    "No package.json found in the current directory. Make sure you are in the project root. If no package.json exists yet, run `npm init` first.",
-  );
-  process.exit(1);
+    'No package.json found in the current directory. Make sure you are in the project root. If no package.json exists yet, run `npm init` first.'
+  )
+  process.exit(1)
 }
 
-const gitignore = fs.readFileSync(path.resolve(__dirname, "gitignore"), "utf8");
+const gitignore = fs.readFileSync(path.resolve(__dirname, 'gitignore'), 'utf8')
 const gitattributes = fs.readFileSync(
-  path.resolve(__dirname, "gitattributes"),
-  "utf8",
-);
+  path.resolve(__dirname, 'gitattributes'),
+  'utf8'
+)
 
 // Config files to write
 const CONFIG_FILES = {
-  ".gitignore": gitignore,
-  ".gitattributes": gitattributes,
-};
+  '.gitignore': gitignore,
+  '.gitattributes': gitattributes,
+}
 
-const failedToWrite = {};
+const failedToWrite = {}
 
 Object.entries(CONFIG_FILES).forEach(([fileName, contents]) => {
   if (!fs.existsSync(fileName)) {
-    log.info(`Writing ${fileName}`);
-    fs.writeFileSync(fileName, contents, "utf8");
+    log.info(`Writing ${fileName}`)
+    fs.writeFileSync(fileName, contents, 'utf8')
   } else if (argv.force) {
-    log.warn(`Overwriting ${fileName}`);
-    fs.writeFileSync(fileName, contents, "utf8");
+    log.warn(`Overwriting ${fileName}`)
+    fs.writeFileSync(fileName, contents, 'utf8')
   } else {
-    log.skip(`${fileName} already exists`);
-    failedToWrite[fileName] = true;
+    log.skip(`${fileName} already exists`)
+    failedToWrite[fileName] = true
   }
-});
+})
 
-log.ok("Done!");
+log.ok('Done!')
